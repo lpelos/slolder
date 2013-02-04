14ee34a07ebfee07e0abf75df25ed59d52a78a78
lpelos <lucas.pelos@gmail.com>
2013-02-04 11:06:00 -0200

    Removing slideshow form de top section & adding examples

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,8 +1,31 @@
 jQuery(function($){
 
-  $(".slolder").slolder({
+  $(".slolder").first().slolder({
     interval: 3000,
     transitionTime: 800
+  });
+
+  $(".slolder.fade-out").slolder({
+    interval: 3000,
+    transitionTime: 800,
+    transitionFunction: "fadeOut"
+  });
+
+  $(".slolder.slide-up").slolder({
+    interval: 3000,
+    transitionTime: 800,
+    transitionFunction: "slideUp"
+  });
+
+  $(".slolder.custom-function").slolder({
+    interval: 3000,
+    transitionFunction: function(slide){
+      return slide.animate({
+        height: "0",
+        opacity: "0",
+        width: "0"
+      }, 500)
+    }
   });
 
 });
